62a8c1bd7520060c899e91390a326f580ac71f63
Bernardo <hummescmpa@gmail.com>
2021-04-08 13:33:49 +0200

    updated to use posestamped for state and continued organization of code

--- src/robot.cpp
+++ src/robot.cpp
@@ -5,26 +5,27 @@
 #include "geometry_msgs/Quaternion.h"
 #include "geometry_msgs/PoseStamped.h"
 
-#include "tiles_loc/State.h"
+#include "std_msgs/Float32.h"
+#include "std_msgs/Float32MultiArray.h"
+
 #include "tiles_loc/Observation.h"
 #include "tiles_loc/Cmd.h"
 
 #include "tf/tf.h"
 
-#include "std_msgs/Float32.h"
-#include "std_msgs/Float32MultiArray.h"
-
 #include <math.h>
-
-float w_x=0, w_y=0, w_th=0;
-float cmd_r, cmd_l;
 
 double sawtooth(double x);
 
-void waypoint_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
 void cmd_callback(const tiles_loc::Cmd::ConstPtr& msg);
 
-void integration_euler(double &x1, double &x2, double &x3, double &x4, double u1, double u2, double dt);
+void image_callback(const sensor_msgs::ImageConstPtr& msg);
+
+void state_loc_callback(const geometry_msgs::PoseStamped::ConstPtr& msg);
+
+void integration_euler(double &x1, double &x2, double &x3, double u1, double u2, double dt);
+
+float cmd_1, cmd_2;
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "robot_node");
@@ -33,10 +34,9 @@ int main(int argc, char **argv) {
   ros::Rate loop_rate(25);  // 25Hz frequency
   double dt = 0.005;  // time step
 
-  double u1 = cmd_r;
-  double u2 = cmd_l;
-
-  double x1, x2, x3, x4;
+  double x1, x2, x3;  // current state of the robot
+  double y1, y2, y3;  // current observation of the robot
+  double u1, u2;      // current input received
 
   // read initial values from the launcher
   n.param<double>("pos_x_init", x1, 0);
@@ -48,30 +48,25 @@ int main(int argc, char **argv) {
   ros::Subscriber sub_cmd = n.subscribe("cmd", 1000, cmd_callback);
 
   // subscriber to the image from the simulator
-//  ros::Subscriber sub_img = n.subscribe("image", 1000, image_callback);
+  ros::Subscriber sub_img = n.subscribe("image", 1000, image_callback);
 
   // subscriber to the updated state from the localization subsystem
-  ros::Subscriber sub_loc = n.subscribe("state_loc", 1000, x_loc_callback);
+  ros::Subscriber sub_loc = n.subscribe("state_loc", 1000, state_loc_callback);
   // ------------------ //
 
   // --- publishers --- //
-  // publisher of the robot's state for control and viewer
-  ros::Publisher pub_state = n.advertise<tiles_loc::State>("state", 1000);
+  // publisher of the state for control and viewer
+  ros::Publisher pub_state = n.advertise<geometry_msgs::PoseStamped::ConstPtr>("state", 1000);
 
-  // publisher of the robot's predicted state and observation for localization
-  ros::Publisher pub_state_pred = n.advertise<tiles_loc::State>("state_pred", 1000);
-  ros::Publisher pub_y = n.advertise<tiles_loc::Observation>("y", 1000);
+  // publisher of the predicted state and observation for localization
+  ros::Publisher pub_state_pred = n.advertise<geometry_msgs::PoseStamped::ConstPtr>("state_pred", 1000);
+  ros::Publisher pub_y = n.advertise<tiles_loc::Observation>("observation", 1000);
   // ------------------ //
 
-  // publi
-
-  ros::Publisher pub_cmd_l;
-  ros::Publisher pub_cmd_r;
-
   while (ros::ok()) {
-    std::cout << "inicio do loop" << std::endl;
-    u1 = cmd_r;
-    u2 = cmd_l;
+    x1 = x_loc_1, x2 = x_loc_2, x3 = x_loc_3;  // start with the last state contracted from the localization
+    y1 = obs_1, y2 = obs_2, y3 = obs3;  // use last observed parameters from the image
+    u1 = cmd_1, u2 = cmd_2;  // use last input from command
 
     if (DATA_FROM_SIM == false) { // evolve model
       std::cout << "entrei no caso com simulacao" << std::endl;
@@ -85,7 +80,6 @@ int main(int argc, char **argv) {
       x1 = lx;
       x2 = ly;
       x3 = compass;
-      x4 = vit;
 
       prev_x1 = x1;
       prev_x2 = x2;
@@ -119,19 +113,18 @@ void waypoint_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
   w_x = msg->pose.position.x;
   w_y = msg->pose.position.y;
   w_th = tf::getYaw(msg->pose.orientation);
-  ROS_INFO("received waypoint: [%f] [%f] [%f]", w_x, w_y, w_th);
+  ROS_INFO("Received waypoint: [%f] [%f] [%f]", w_x, w_y, w_th);
 }
 
 void cmd_callback(const tiles_loc::Cmd::ConstPtr& msg) {
-  cmd1 = msg->data.u1;
-  cmd2 = msg->data.u2;
-  ROS_INFO("received command: u1 [%f] u2 [%f]", cmd1, cmd2);
+  cmd_1 = msg->data.u1;
+  cmd_2 = msg->data.u2;
+  ROS_INFO("Received command: u1 [%f] u2 [%f]", cmd_1, cmd_2);
 }
 
-void integration_euler(double &x1, double &x2, double &x3, double &x4, double u1, double u2, double dt) {
-  std::cout << "x1: " << x1 << "| x2: "  << x2 << " | x3: " << x3 << " | x4 " << x4 << std::endl;
-  x1 = x1 + dt * (x4*cos(x3));
-  x2 = x2 + dt * (x4*sin(x3));
-  x3 = x3 + dt * (P1*(u1-u2));
-  x4 = x4 + dt * (P2*(u1+u2) + P3*x4*abs(x4));
+void integration_euler(double &x1, double &x2, double &x3, double u1, double u2, double dt) {
+  x1 = x1 + dt * (u1*cos(x3));
+  x2 = x2 + dt * (u1*sin(x3));
+  x3 = x3 + dt * (u2);
+  ROS_INFO("Updated state -> x1: [%f] | x2: [%f] | x3: [%f] || u1: [%f] | u2: [%f]", x1, x2, x3, u1, u2);
 }
