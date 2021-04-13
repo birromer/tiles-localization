9ee8c25e1413f47cfc65d1d0ec5ef2aebf70245c
Bernardo <hummescmpa@gmail.com>
2021-04-13 15:36:26 +0200

    documented and and small refactor of viewer node

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -1,3 +1,15 @@
+/*
+** This node is responsible for the visualization of the robot's state, wapoint and estimation.
+**
+** Subscribers:
+**   - geometry_msgs::PoseStamped waypoint  // the target state, waypoint
+**   - geometry_msgs::PoseStamped pose      // the current pose, ground truth
+**   - geometry_msgs::PoseStamped state     // the estimated state
+**
+** Publishers:
+**   - none
+ */
+
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <opencv2/highgui.hpp>
@@ -16,32 +28,31 @@
 #include <ibex.h>
 #include <tubex.h>
 #include <tubex-rob.h>
-//#include <tubex-3rd.h>
 
 using namespace cv;
 using namespace std;
 using namespace ibex;
 using namespace tubex;
 
-void consigneCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
-  float cons_x, cons_y, cons_cap;
-  cons_x = msg->pose.position.x;
-  cons_y = msg->pose.position.y;
-  cons_cap = tf::getYaw(msg->pose.orientation);
+void waypoint_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+  float w_x, w_y, w_cap;
+  w_x = msg->pose.position.x;
+  w_y = msg->pose.position.y;
+  w_th = tf::getYaw(msg->pose.orientation);
 
-  vibes::drawVehicle(cons_x, cons_y, cons_cap*180./M_PI, 0.2, "green");
+  vibes::drawVehicle(w_x, w_y, w_th*180./M_PI, 0.2, "green");
 }
 
-void stateCallback(const geometry_msgs::PoseStamped::ConstPtr& msg){
-  float x, y, c;
-  x = msg->pose.position.x;
-  y = msg->pose.position.y;
-  c = tf::getYaw(msg->pose.orientation);
+void state_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
+  float x_x, x_y, x_th;
+  x_x = msg->pose.position.x;
+  x_y = msg->pose.position.y;
+  x_th = tf::getYaw(msg->pose.orientation);
 
-  vibes::drawVehicle(x, y, c*180./M_PI, 0.4, "blue");
+  vibes::drawVehicle(x_x, x_y, x_th*180./M_PI, 0.4, "blue");
 }
 
-void poseCallback(const geometry_msgs::Pose& msg){
+void pose_callback(const geometry_msgs::Pose& msg){
   float x, y, c;
   x = msg.position.x;
   y = msg.position.y;
@@ -58,13 +69,13 @@ int main(int argc, char **argv){
 
   fig_map.show();
 
-  ros::init(argc, argv, "Viewer_node");
+  ros::init(argc, argv, "viewer_node");
 
   ros::NodeHandle n;
 
-  ros::Subscriber sub_consigne = n.subscribe("consigne", 1000, consigneCallback);
-  ros::Subscriber sub_state = n.subscribe("state_estimated", 1000, stateCallback);
-  ros::Subscriber sub_pose = n.subscribe("pose", 1000, poseCallback);
+  ros::Subscriber sub_waypoint = n.subscribe("waypoint", 1000, waypoint_callback);
+  ros::Subscriber sub_state = n.subscribe("state", 1000, state_callback);
+  ros::Subscriber sub_pose = n.subscribe("pose", 1000, pose_callback);
 
   ros::spin();
 
