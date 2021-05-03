7bdb8c9a2f6bc9a3dd616ec820de85ab6532ae07
Bernardo Hummes <hummescmpa@gmail.com>
2021-05-03 15:14:15 +0200

    trying to display multiple images in the same window

--- src/robot.cpp
+++ src/robot.cpp
@@ -40,6 +40,7 @@
 #include <codac.h>
 #include <codac-rob.h>
 #include <math.h>
+#include <stdarg.h>
 
 using namespace cv;
 
@@ -58,6 +59,7 @@ double modulo(double a, double b);
 int sign(double x);
 double median(std::vector<double> scores);
 ibex::IntervalVector integration_euler(ibex::IntervalVector state, double u1, double u2, double dt);
+void ShowManyImages(string title, int nArgs, ...);
 
 // message convertion functions
 tiles_loc::State state_to_msg(double x1, double x2, double x3);
@@ -126,8 +128,9 @@ int main(int argc, char **argv) {
 
   // start visualization windows windows
   if(display_window) {
+    cv::namedWindow("steps");
 //    cv::namedWindow("lines");
-    cv::namedWindow("camera");
+//    cv::namedWindow("camera");
 //    cv::namedWindow("grey");
 //    cv::namedWindow("sobel");
 //    cv::namedWindow("canny");
@@ -495,7 +498,8 @@ void image_callback(const sensor_msgs::ImageConstPtr& msg) {
       obs_3 = alpha_median;
 
       if(display_window){
-        cv::imshow("camera", in);
+        ShowManyImages("steps", 6, in, grey, grad, edges, src, rot);
+//        cv::imshow("camera", in);
 //        cv::imshow("grey", grey);
 //        cv::imshow("sobel", grad);
 //        cv::imshow("canny", edges);
@@ -513,8 +517,117 @@ void image_callback(const sensor_msgs::ImageConstPtr& msg) {
     ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
     return;
   }
+}
 
+void ShowManyImages(string title, int nArgs, ...) {
+// from https://github.com/opencv/opencv/wiki/DisplayManyImages
 
+  int size;
+  int i;
+  int m, n;
+  int x, y;
+
+  // w - Maximum number of images in a row
+  // h - Maximum number of images in a column
+  int w, h;
+
+  // scale - How much we have to resize the image
+  float scale;
+  int max;
+
+  // If the number of arguments is lesser than 0 or greater than 12
+  // return without displaying
+  if(nArgs <= 0) {
+      printf("Number of arguments too small....\n");
+      return;
+  }
+  else if(nArgs > 14) {
+      printf("Number of arguments too large, can only handle maximally 12 images at a time ...\n");
+      return;
+  }
+  // Determine the size of the image,
+  // and the number of rows/cols
+  // from number of arguments
+  else if (nArgs == 1) {
+      w = h = 1;
+      size = 300;
+  }
+  else if (nArgs == 2) {
+    w = 2; h = 1;
+      size = 300;
+  }
+  else if (nArgs == 3 || nArgs == 4) {
+      w = 2; h = 2;
+      size = 300;
+  }
+  else if (nArgs == 5 || nArgs == 6) {
+      w = 3; h = 2;
+      size = 200;
+  }
+  else if (nArgs == 7 || nArgs == 8) {
+      w = 4; h = 2;
+      size = 200;
+  }
+  else {
+      w = 4; h = 3;
+      size = 150;
+  }
+
+  // Create a new 3 channel image
+  Mat DispImage = Mat::zeros(Size(100 + size*w, 60 + size*h), CV_8UC3);
+
+  // Used to get the arguments passed
+  va_list args;
+  va_start(args, nArgs);
+
+  // Loop for nArgs number of arguments
+  for (i = 0, m = 20, n = 20; i < nArgs; i++, m += (20 + size)) {
+      // Get the Pointer to the IplImage
+      Mat img = va_arg(args, Mat);
+
+      // Check whether it is NULL or not
+      // If it is NULL, release the image, and return
+      if(img.empty()) {
+          printf("Invalid arguments");
+          return;
+      }
+
+      // Find the width and height of the image
+      x = img.cols;
+      y = img.rows;
+
+      // Find whether height or width is greater in order to resize the image
+      max = (x > y)? x: y;
+
+      // Find the scaling factor to resize the image
+      scale = (float) ( (float) max / size );
+
+      // Used to Align the images
+      if( i % w == 0 && m!= 20) {
+          m = 20;
+          n+= 20 + size;
+      }
+
+      // Set the image ROI to display the current image
+      // Resize the input image and copy the it to the Single Big Image
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 1" << std::endl;
+      Rect ROI(m, n, (int)( x/scale ), (int)( y/scale ));
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 2" << std::endl;
+      Mat temp; resize(img, temp, Size(ROI.width, ROI.height));
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 3" << std::endl;
+      temp.copyTo(DispImage(ROI));
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 4" << std::endl;
+  }
+
+  // Create a new window, and show the Single Big Image
+//  namedWindow( title, 1 );
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 5" << std::endl;
+  imshow(title, DispImage);
+  std::cout << "REACHED HEEEEEEEEEEEEEEEEEEEEERE 6" << std::endl;
+//  waitKey();
+
+  // End the number of arguments
+  va_end(args);
 }
 
 void pose_callback(const geometry_msgs::Pose::ConstPtr& msg) {
