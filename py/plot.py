844bf7942c17d8dad41e5884a54e0e709e4c0480
Bernardo Hummes <hummescmpa@gmail.com>
2021-06-10 12:04:43 +0200

    added plot

--- py/plot.py
+++ py/plot.py
@@ -15,10 +15,13 @@ if __name__ == "__main__":
 
   data = pd.read_csv(file_path)
 
-  figure, ax1 = plt.subplots()
+  fig, axes = plt.subplots(nrows=2, ncols=1)
 
-  #ax1.plot(data.iloc[:,0], data.iloc[:,1], data.iloc[:,2], label="Sim1")
-  ax1.plot(data,label="Sim1")
-#  ax1.plot(data.iloc[:,3], data.iloc[:,4], data.iloc[:,5], label="Sim2")
-  ax1.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+  print(data)
+
+  data[["sim1_eq1", "sim1_eq2", "sim1_eq3"]].plot(ax=axes[0], label="Sim1")
+  data[["sim2_eq1", "sim2_eq2", "sim2_eq3"]].plot(ax=axes[1], label="Sim2")
+  axes[0].legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+  axes[1].legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
+
   plt.show()
