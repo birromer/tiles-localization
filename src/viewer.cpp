eed7c0eebc5cfae5544a75e837d888635b349391
Bernardo Hummes <hummescmpa@gmail.com>
2021-08-18 10:49:48 +0200

    added viz stuff from root, nothing being displayed

--- src/viewer.cpp
+++ src/viewer.cpp
@@ -60,6 +60,18 @@ ofstream file_eq_yx;
 ofstream file_eq_yp;
 //ofstream file_gt;
 
+auto c1 = std::make_unique<TCanvas>("c1", "Equivalence equations");
+auto f1 = std::make_unique<TGraph>(num_imgs);
+auto f2 = std::make_unique<TGraph>(num_imgs);
+auto f3 = std::make_unique<TGraph>(num_imgs);
+auto f4 = std::make_unique<TGraph>(num_imgs);
+auto f5 = std::make_unique<TGraph>(num_imgs);
+auto f6 = std::make_unique<TGraph>(num_imgs);
+auto f7 = std::make_unique<TGraph>(num_imgs);
+vector<vector<double>> sim_data;
+vector<vector<double>> sim_ground_truth;
+int curr_img = 0;
+
 void waypoint_callback(const geometry_msgs::PoseStamped::ConstPtr& msg){
   float w_x, w_y, w_th;
   w_x = msg->pose.position.x;
@@ -103,86 +115,122 @@ void observation_callback(const tiles_loc::Observation::ConstPtr& msg) {
   ibex::IntervalVector x = state_loc;
   ibex::IntervalVector y = observation;
 
+  double expected_1 = (pose_1/TILE_SIZE - floor(pose_1/TILE_SIZE))*TILE_SIZE;
+  double expected_2 = (pose_2/TILE_SIZE - floor(pose_2/TILE_SIZE))*TILE_SIZE;
+  double expected_3 = ((pose_3-M_PI/4)/(M_PI/2) - floor((pose_3-M_PI/4)/(M_PI/2))-0.5) * 2*M_PI/4;  // modulo without function as it is in radians
+
   ROS_WARN("[VIEWER] Using state: x1 [%f] | x2 [%f] | x3 [%f]", x[0].mid(), x[1].mid(), x[2].mid());
   ROS_WARN("[VIEWER] Using parameters: y1 [%f] | y2 [%f] | y3 [%f]", y[0].mid(), y[1].mid(), y[2].mid());
   ROS_WARN("[VIEWER] Using truth: p1 [%f] | p2 [%f] | p3 [%f]", pose_1, pose_2, pose_3);
 
-  // comparing Y with X
-  double sim1_eq1 = sin(M_PI*(y[0].mid()-x[0].mid())/TILE_SIZE);
-  double sim1_eq2 = sin(M_PI*(y[1].mid()-x[1].mid())/TILE_SIZE);
-  double sim1_eq3 = sin(y[2].mid()-x[2].mid());
+  // ground truth and parameters should have near 0 value in the equivalency equations
+  double sim1_eq1 = sin(M_PI*(pose_1-y[0].mid())/TILE_SIZE);
+  double sim1_eq2 = sin(M_PI*(pose_2-y[1].mid())/TILE_SIZE);
+  double sim1_eq3 = sin(pose_3-y[2].mid());
 
-  double sim2_eq1 = sin(M_PI*(y[0].mid()-x[1].mid())/TILE_SIZE);
-  double sim2_eq2 = sin(M_PI*(y[1].mid()-x[0].mid())/TILE_SIZE);
-  double sim2_eq3 = cos(y[2].mid()-x[2].mid());
-
-  file_eq_yx << sim1_eq1 << "," << sim1_eq2 << "," << sim1_eq3 << "," << sim2_eq1 << "," << sim2_eq2 << "," << sim2_eq3 << endl;
-
-  ROS_INFO("[VIEWER] Equivalence equations 1:\nsin(pi*(y1-z1)) = [%f]\nsin(pi*(y2-z2)) = [%f]\nsin(y2-z2) = [%f]\n", sim1_eq1, sim1_eq2, sim1_eq3);
-  ROS_INFO("[VIEWER] Equivalence equations 2:\nsin(pi*(y1-z2)) = [%f]\nsin(pi*(y2-z1)) = [%f]\ncos(y2-z1) = [%f]\n", sim2_eq1, sim2_eq2, sim2_eq3);
-
-  // comparing Y with pose
-  sim1_eq1 = sin(M_PI*(y[0].mid()-pose_1));
-  sim1_eq2 = sin(M_PI*(y[1].mid()-pose_2));
-  sim1_eq3 = sin(y[2].mid()-pose_3);
-
-  sim2_eq1 = sin(M_PI*(y[0].mid()-pose_2));
-  sim2_eq2 = sin(M_PI*(y[1].mid()-pose_1));
-  sim2_eq3 = cos(y[2].mid()-pose_3);
+  double sim2_eq1 = sin(M_PI*(pose_2-y[0].mid())/TILE_SIZE);
+  double sim2_eq2 = sin(M_PI*(pose_1-y[1].mid())/TILE_SIZE);
+  double sim2_eq3 = cos(pose_3-y[2].mid());
 
   file_eq_yp << sim1_eq1 << "," << sim1_eq2 << "," << sim1_eq3 << "," << sim2_eq1 << "," << sim2_eq2 << "," << sim2_eq3 << endl;
 
   ROS_INFO("[VIEWER] Equivalence equations 1:\nsin(pi*(y1-z1)) = [%f]\nsin(pi*(y2-z2)) = [%f]\nsin(y2-z2) = [%f]\n", sim1_eq1, sim1_eq2, sim1_eq3);
   ROS_INFO("[VIEWER] Equivalence equations 2:\nsin(pi*(y1-z2)) = [%f]\nsin(pi*(y2-z1)) = [%f]\ncos(y2-z1) = [%f]\n", sim2_eq1, sim2_eq2, sim2_eq3);
 
-//  file_gt << pose_1 << "," << pose_2 << "," << pose_3 << endl;
-
   // plot similarity equations
+  vector<double> s{sim1_eq1, sim1_eq2, sim1_eq3, sim2_eq1, sim2_eq2, sim2_eq3};
+
+  if (sim_data.size() == curr_img){
+    sim_data.push_back(s);
+  } else {
+    sim_data.at(curr_img) = s;
+  }
+
+  // redraw graph
+  for (int i=0; i < curr_img; i++) {
+    if (i < curr_img) {
+      f1->SetPoint(i, i, sim_data[i][0]);
+      f2->SetPoint(i, i, sim_data[i][1]);
+      f3->SetPoint(i, i, sim_data[i][2]);
+      f4->SetPoint(i, i, sim_data[i][3]);
+      f5->SetPoint(i, i, sim_data[i][4]);
+      f6->SetPoint(i, i, sim_data[i][5]);
+    } else {
+      f1->SetPoint(i, i, 0);
+      f2->SetPoint(i, i, 0);
+      f3->SetPoint(i, i, 0);
+      f4->SetPoint(i, i, 0);
+      f5->SetPoint(i, i, 0);
+      f6->SetPoint(i, i, 0);
+    }
+  }
+  f1->RemovePoint(curr_img+1);
+  f2->RemovePoint(curr_img+1);
+  f3->RemovePoint(curr_img+1);
+  f4->RemovePoint(curr_img+1);
+  f5->RemovePoint(curr_img+1);
+  f6->RemovePoint(curr_img+1);
+
+  // notify ROOT that the plots have been modified and needs update
+  c1->cd(1);
+  c1->Update();
+  c1->Pad()->Draw();
+  c1->cd(2);
+  c1->Update();
+  c1->Pad()->Draw();
+  c1->cd(3);
+  c1->Update();
+  c1->Pad()->Draw();
+  c1->cd(4);
+  c1->Update();
+  c1->Pad()->Draw();
+  c1->cd(5);
+  c1->Update();
+  c1->Pad()->Draw();
+  c1->cd(6);
+  c1->Update();
+  c1->Pad()->Draw();
+
+  curr_img += 1;
 }
 
 int main(int argc, char **argv){
   // ----------------- ROOT setup ----------------- //
   TApplication rootapp("viz", &argc, argv);
 
-  auto c1 = std::make_unique<TCanvas>("c1", "Equivalence equations");
+  // c1, f1, f2, f3, f4, f5, f6 declared globally
   c1->SetWindowSize(1550, 700);
 
-  auto f1 = std::make_unique<TGraph>(num_imgs);
   f1->SetTitle("sin(pi*(y1-z1)/tile_size)");
   f1->GetXaxis()->SetTitle("Iteration");
   f1->GetYaxis()->SetTitle("Similarity score");
   f1->SetMinimum(-1);
   f1->SetMaximum(1);
 
-  auto f2 = std::make_unique<TGraph>(num_imgs);
   f2->SetTitle("sin(pi*(y2-z2)/tile_size)");
   f2->GetXaxis()->SetTitle("Iteration");
   f2->GetYaxis()->SetTitle("Similarity score");
   f2->SetMinimum(-1);
   f2->SetMaximum(1);
 
-  auto f3 = std::make_unique<TGraph>(num_imgs);
   f3->SetTitle("sin(y3-z3)");
   f3->GetXaxis()->SetTitle("Iteration");
   f3->GetYaxis()->SetTitle("Similarity score");
   f3->SetMinimum(-1);
   f3->SetMaximum(1);
 
-  auto f4 = std::make_unique<TGraph>(num_imgs);
   f4->SetTitle("sin(pi*(y1-z2)/tile_size)");
   f4->GetXaxis()->SetTitle("Iteration");
   f4->GetYaxis()->SetTitle("Similarity score");
   f4->SetMinimum(-1);
   f4->SetMaximum(1);
 
-  auto f5 = std::make_unique<TGraph>(num_imgs);
   f5->SetTitle("sin(pi*(y2-z1)/tile_size)");
   f5->GetXaxis()->SetTitle("Iteration");
   f5->GetYaxis()->SetTitle("Similarity score");
   f5->SetMinimum(-1);
   f5->SetMaximum(1);
 
-  auto f6 = std::make_unique<TGraph>(num_imgs);
   f6->SetTitle("cos(y3-z3)");
   f6->GetXaxis()->SetTitle("Iteration");
   f6->GetYaxis()->SetTitle("Similarity score");
@@ -216,11 +264,11 @@ int main(int argc, char **argv){
   // ------------------------------------------------ //
 
   file_eq_yx.open("/home/birromer/ros/data_tiles/temp/eq_yx.csv", fstream::in | fstream::out | fstream::trunc);
-  file_eq_yp.open("/home/birromer/ros/data_tiles/temp/eq_yp.csv", fstream::in | fstream::out | fstream::trunc);
+//  file_eq_yp.open("/home/birromer/ros/data_tiles/temp/eq_yp.csv", fstream::in | fstream::out | fstream::trunc);
 //  file_gt.open("/home/birromer/ros/data_tiles/temp/gt.csv", fstream::in | fstream::out | fstream::trunc);
 
   file_eq_yx << "sim1_eq1" << "," << "sim1_eq2" << "," << "sim1_eq3" << "," << "sim2_eq1" << "," << "sim2_eq2" << "," << "sim2_eq3" << endl;
-  file_eq_yp << "sim1_eq1" << "," << "sim1_eq2" << "," << "sim1_eq3" << "," << "sim2_eq1" << "," << "sim2_eq2" << "," << "sim2_eq3" << endl;
+//  file_eq_yp << "sim1_eq1" << "," << "sim1_eq2" << "," << "sim1_eq3" << "," << "sim2_eq1" << "," << "sim2_eq2" << "," << "sim2_eq3" << endl;
 //  file_gt << "x" << "," << "y" << "," << "theta" << endl;
 
   ros::init(argc, argv, "viewer_node");
